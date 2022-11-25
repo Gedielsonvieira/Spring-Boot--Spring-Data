94165bc6135fb1926a9dd03d8b33f66a86ec4472
Gedielson Vieira <gedielsonvieira@outlook.com>
2022-11-25 11:43:29 -0300

    Mais relacionamentos - Mapeando novas entidades

---
+++ Java-e-JPA-consultas-avancadas-performance-e-modelos-complexos/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,56 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate data = LocalDate.now();
+    private BigDecimal valorTotal;
+    @ManyToOne
+    private Cliente clienteId;
+
+    public Pedido(Cliente clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public Pedido() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Cliente getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Cliente clienteId) {
+        this.clienteId = clienteId;
+    }
+}
